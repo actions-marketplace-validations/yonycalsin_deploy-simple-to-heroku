ace19eda4b0b975c5015b7bb1b3d64d47eeb3c2e
Yony Calsin <helloyonycalsin@gmail.com>
2022-05-25 16:06:12 -0500

    chore: build source code

--- dist/index.js
+++ dist/index.js
@@ -1955,6 +1955,14 @@ module.exports = require("assert");
 
 /***/ }),
 
+/***/ 81:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("child_process");
+
+/***/ }),
+
 /***/ 361:
 /***/ ((module) => {
 
@@ -2068,11 +2076,75 @@ module.exports = require("util");
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
+const { execSync } = __nccwpck_require__(81)
 const core = __nccwpck_require__(186)
 
+function createNetrcFileCommand(config) {
+  const { heroku_api_key, heroku_email } = config
+
+  return `cat > ~/.netrc <<EOF
+  machine api.heroku.com
+      login ${heroku_email}
+      password ${heroku_api_key}
+  machine git.heroku.com
+      login ${heroku_email}
+      password ${heroku_api_key}
+  EOF`
+}
+
+function addHerokuRemoteCommand(config) {
+  const { heroku_app_name } = config
+
+  // GitHub Actions does come with the heroku cli pre-installed
+  return `heroku git:remote --app ${heroku_app_name}`
+}
+
+function deployToHerokuCommand() {
+  // TODO: Add support for other branches
+  return `git push heroku master:refs/heads/master &>/dev/null &`
+}
+
 async function main() {
+  const config = {
+    heroku_api_key: core.getInput('heroku_api_key'),
+    heroku_email: core.getInput('heroku_email'),
+    heroku_app_name: core.getInput('heroku_app_name'),
+  }
+
+  core.debug(`Config: ${JSON.stringify(config)}`)
+
   try {
-    core.setOutput('status', 'Successfully deployed app')
+    // Check if Repo clone is shallow
+    const isShallow = execSync('git rev-parse --is-shallow-repository').toString()
+
+    core.info(`The repo clone is shallow: ${isShallow}`)
+
+    // If the Repo clone is shallow, make it unshallow
+    if (isShallow === 'true\n') {
+      execSync('git fetch --prune --unshallow', {
+        stdio: 'inherit',
+      })
+    }
+
+    execSync(createNetrcFileCommand(config), {
+      stdio: 'inherit',
+    })
+
+    core.info('Successfully logged into heroku')
+
+    execSync(addHerokuRemoteCommand(config), {
+      stdio: 'inherit',
+    })
+
+    core.info('Added git remote heroku')
+
+    await execSync(deployToHerokuCommand(), {
+      stdio: 'inherit',
+    })
+
+    core.info(`${config.heroku_app_name} successfully deployed on heroku`)
+
+    core.setOutput('status', `${config.heroku_app_name} successfully deployed on heroku`)
   } catch (error) {
     core.setFailed(error.toString())
   }
