df6e3e1e2aa6adee5620c7aeca8c8748b56c93be
Yony Calsin <helloyonycalsin@gmail.com>
2022-05-25 15:43:10 -0500

    feat: save the heroku credentials in ~/.netrc

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
+const { execSync } = require('child_process')
 const core = require('@actions/core')
+
+function createNetrcFileCommand(config) {
+  const { heroku_api_key, heroku_email } = config
+
+  return `cat > ~/.netrc <<EOF
+  machine api.heroku.com
+      login ${heroku_email}
+      password ${heroku_api_key}
+  machine git.heroku.com
+      login ${heroku_email}
+      password ${heroku_api_key}
+  EOF`
+}
 
 async function main() {
   try {
-    core.setOutput('status', 'Successfully deployed app')
+    execSync(createNetrcFileCommand(config), {
+      stdio: 'inherit',
+    })
+
+    core.info('Successfully logged into heroku')
   } catch (error) {
     core.setFailed(error.toString())
   }
